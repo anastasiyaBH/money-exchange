429cf629ff947ed6e3240e2a5d5e57b78c546a4a
AnastasiyaBH <bhanastasia495@gmail.com>
2019-03-05 23:16:30 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return {
+            error: 'You are rich, my friend! We don\'t have so much coins for exchange'
+        };
+    }
+    var exchange = {};
+
+    if (currency <= 0){
+        return exchange;
+    }
+    
+    var coinDenomination = {
+        'H': 50,
+        'Q': 25,
+        'D': 10,
+        'N': 5,
+        'P': 1 
+    };
+    
+    for (var key in coinDenomination) {
+       var qt = Math.floor(currency / coinDenomination[key]);
+       if(qt > 0) {
+        exchange[key] = qt;
+        currency = currency % coinDenomination[key];
+       }     
+    }
+    return exchange;
 }
